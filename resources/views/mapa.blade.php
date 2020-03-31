29562de7f582c8687df21a543aaf09f96071176e
victor mussel <victor.mussel@hotmail.com>
2020-03-31 11:04:32 -0300

    'nada'

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -2,20 +2,24 @@
 
 
 @section('content')
- 
-<div id="map"></div>
-    
-    
+
+<style>
+    /* Always set the map height explicitly to define the size of the div
+     * element that contains the map. */
+    #map {
+      height: 100%;
+    }
+    /* Optional: Makes the sample page fill the window. */
+    html, body {
+      height: 100%;
+      margin: 0;
+      padding: 0;
+    }
+  </style>
+<div class="x_panel modal-content ">
+   <div></div>
+ </div>
+
 
 @endsection
 
-@push('scripts')
-
-
-
-<script type="text/javascript">
-   
-</script>
-
-
-@endpush
